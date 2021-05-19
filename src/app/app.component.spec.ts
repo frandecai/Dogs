049bf0e986818cd0d577ac48467e564d23ec82a3
Francisco Javier Torres Ponce <francisco-javier.torres-ponce@capgemini.com>
2021-05-19 17:40:07 +0200

    Refactor of app.component.spec.ts and another test more

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+    let app: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
@@ -16,22 +19,36 @@ describe('AppComponent', () => {
         }).compileComponents();
     }));
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('Should create the app', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'Dogs'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
+    it(`Should have as title 'Dogs'`, () => {
         expect(app.title).toEqual('Dogs');
     });
 
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
+    it('Should render title in a h1 tag', () => {
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain('Dogs');
+    });
+
+    it('Should have a list of three options to choose', () => {
+        const pageHTML: HTMLElement = fixture.nativeElement;
+        const listHTML = pageHTML.querySelector('.list-group');
+        expect(listHTML).toBeTruthy();
+        if(listHTML) {
+            let firstElementListHTML = listHTML.getElementsByClassName('list-group-item')[0];
+            let secondElementListHTML = listHTML.getElementsByClassName('list-group-item')[1];
+            let thirdElementListHTML = listHTML.getElementsByClassName('list-group-item')[2];
+            expect(firstElementListHTML.textContent).toEqual('A single dog');
+            expect(secondElementListHTML.textContent).toEqual('By breed');
+            expect(thirdElementListHTML.textContent).toEqual('By breed & sub-breed');
+        }
     });
 });
