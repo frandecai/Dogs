67d7f11e8d10e09d940c0ead7a3fd2c84b710dca
Francisco Javier Torres Ponce <francisco-javier.torres-ponce@capgemini.com>
2021-05-19 13:03:24 +0200

    Code correction for first tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule,
+                HttpClientTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'Dogs'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Dogs');
-  });
+    it(`should have as title 'Dogs'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('Dogs');
+    });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Dogs!');
-  });
+    it('should render title in a h1 tag', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Dogs');
+    });
 });
--- src/app/services/dog/dog.service.spec.ts
+++ src/app/services/dog/dog.service.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DogService } from './dog.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DogService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    let httpClientSpy: { get: jasmine.Spy };
+    let dogService: DogService;
+
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [DogService],
+        imports: [HttpClientTestingModule]
+    }));
 
     it('should be created', () => {
         const service: DogService = TestBed.get(DogService);
         expect(service).toBeTruthy();
     });
+
+    // it('#findAllBreedsReponse should return value from observable',
+    //     (done: DoneFn) => {
+    //         dogService.findAllBreedsReponse().subscribe(value => {
+    //         expect(value).toBe('JSON value');
+    //         done();
+    //     });
+    // });
 });
